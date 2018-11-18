f039c1f4cc2cd4596330c591e444ed8598a3410d
FightingSprrow <kbauertx@gmail.com>
2018-11-18 16:51:24 -0600

    added courasel as portfolio

--- assets/js/app.js
+++ assets/js/app.js
@@ -14,7 +14,45 @@
   // });
 
   $(document).ready(function(){
+    $('.carousel').carousel();
     $('.sidenav').sidenav();
     $('.parallax').parallax();
     $('.materialboxed').materialbox();
+    
+
+
+  // Copy my email address to clipboard so that address isn't on website.
+    $('#email-address').click(function(){
+      var email = "kbauertx@gmail.com"
+      copyTextToClipboard(email);
+    });
+    
+
+    function copyTextToClipboard(text) {
+      var textArea = document.createElement("textarea");
+
+      textArea.style.position = 'fixed';
+      textArea.style.top = 0;
+      textArea.style.left = 0;
+      textArea.style.width = '2em';
+      textArea.style.height = '2em';
+      textArea.style.padding = 0;
+      textArea.style.border = 'none';
+      textArea.style.outline = 'none';
+      textArea.style.boxShadow = 'none';
+      textArea.style.background = 'transparent';
+      textArea.value = text;
+      document.body.appendChild(textArea);
+      textArea.select();
+      try {
+        var successful = document.execCommand('copy');
+        var msg = successful ? 'successful' : 'unsuccessful';
+        console.log('Copying text command was ' + msg);
+      } catch (err) {
+        console.log('Oops, unable to copy');
+      }
+      document.body.removeChild(textArea);
+    }
+    
+
   });
