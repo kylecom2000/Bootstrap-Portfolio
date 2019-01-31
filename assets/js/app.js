7ab13d9e15d13f9980a002fae290292bca67e2f2
FightingSprrow <kbauertx@gmail.com>
2019-01-31 12:27:50 -0600

    updated portfolio

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,18 +1,3 @@
-
-  // document.addEventListener('DOMContentLoaded', function() {
-  //   var elems = document.querySelectorAll('.sidenav');
-  //   var instances = M.Sidenav.init(elems, options);
-  // });
-
-
-  // document.addEventListener('DOMContentLoaded', function() {
-  //   M.AutoInit();
-  //   var elems = document.querySelectorAll('.parallax');
-  //   var instances = M.Parallax.init(elems);
-  //   $('.sidenav').sidenav();
-    
-  // });
-
   $(document).ready(function(){
     $('.slider').slider();
     $('.collapsible').collapsible();
